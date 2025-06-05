af29fe65e7f97896e766ed6810e8eb3ec804ffed
livctr <vhli2020@gmail.com>
2025-06-05 14:37:09 -0400

    working video annotation pipeline

--- data/visualization/examples.py
+++ data/visualization/examples.py
@@ -1,55 +1,68 @@
 from data.visualization import (
-    BoxRenderer,
-    COCOKeypoints3DRenderer,
-    COCOKeypointsRenderer,
     FrameRenderer,
+    HorizontalLabelBarRenderer,
     ProgressRenderer,
     TextRenderer,
     DecordVideoStreamer,
     ProgressStreamer,
     StaticStreamer,
-    StaticHorizontalLabelBarStreamer,
+    StaticTabularStreamer,
     TabularStreamer,
-    LeafNode,
-    CompositeNode,
-    VideoAnnotationWriter
+    LeafOrchestrator,
+    CompositeOrchestrator,
+    write_to_file
 )
 
 from data.utils_strokerehab import LabelUtils
 
 
-def write_video_with_label(title, video_path, label_path, output_path, fps):
+def write_video_with_label(title_str, video_path, label_path, output_path, fps):
 
+    handedness = LabelUtils.get_handedness(label_path)
     action_seq = LabelUtils.convert_labels_to_action_sequence(label_path)
     action_seq = {"Time_s": [action[0] for action in action_seq],
-                  "MarkerNames": [action[1] for action in action_seq]}
-
+                  "MarkerNames": [f"Label: {action[1]} ({handedness} hand)"  for action in action_seq]}
+    print(action_seq)
     # Create text nodes for title and labels with larger font
-    title_node = LeafNode("title", TextRenderer(StaticStreamer(title), font_scale=2.0))
-    label_node = LeafNode("label", TextRenderer(TabularStreamer(action_seq, "MarkerNames", "Time_s"), font_scale=2.0))
-
-    # Create progress bar to show position in video timeline
-    progress_node = LeafNode("progress_bar", FrameRenderer(StaticHorizontalLabelBarStreamer(label_path, "MarkerNames", "Time_s")))
-
-    # Set up video frame display
-    frame_streamer = DecordVideoStreamer(video_path, read_from_cpu_id=1)
-    frame_node = LeafNode("video", FrameRenderer(frame_streamer))
-
-    # Add marker to show current position on progress bar
-    progress_marker_node = LeafNode("progress_marker", ProgressRenderer(ProgressStreamer(frame_streamer)))
-
-    # Arrange nodes in a grid layout:
-    # The positions are specified as (layer, row, column)
-    # Layer 0: Title, Label, Progress bar, Video frame
-    # Layer 1: Progress marker
-    root_node = CompositeNode()
-    root_node.add_children(
-        [title_node, label_node, progress_node, frame_node, progress_marker_node],
-        [(0, 0, 0), (0, 1, 0), (0, 2, 0), (0, 3, 0), (1, 2, 0)]
+    title = LeafOrchestrator(
+        "title",
+        StaticStreamer(f"Title: {title_str}"),
+        TextRenderer(font_scale=0.5)
     )
-    root_node.compile(1.0 / fps)
-    writer = VideoAnnotationWriter(root_node)
-    writer.write_to_file(output_path, num_render_workers=1, use_multiprocessing=False)
+    label = LeafOrchestrator(
+        "label",
+        TabularStreamer(action_seq, "MarkerNames", "Time_s", stream_method="nearest_left"),
+        TextRenderer(font_scale=0.5)
+    )
+
+    frame_streamer = DecordVideoStreamer(video_path, read_from_cpu_id=0)
+    frame = LeafOrchestrator(
+        "frame",
+        frame_streamer,
+        FrameRenderer(sized_streamer=frame_streamer)  # Get size info from streamer
+    )
+
+    label_bar = LeafOrchestrator(
+        "label_bar",
+        StaticTabularStreamer(action_seq, "MarkerNames", "Time_s", num_samples=500, subsample_method="nearest_left"),
+        HorizontalLabelBarRenderer(
+            height=20
+        )
+    )
+
+    label_time_marker = LeafOrchestrator(
+        "label_time_marker",
+        ProgressStreamer(frame_streamer),
+        ProgressRenderer()
+    )
+
+    root_orchestrator = CompositeOrchestrator()
+    root_orchestrator.add_children(
+        [title, label, frame, label_bar, label_time_marker],
+        [(0, 0, 0), (0, 1, 0), (0, 3, 0), (0, 2, 0), (1, 2, 0)]
+    )
+    root_orchestrator.compile(sample_rate=1.0 / fps)
+    write_to_file(root_orchestrator, output_path, num_render_workers=4, use_multiprocessing=False, fourcc_str='mp4v')
 
 
 if __name__ == "__main__":
